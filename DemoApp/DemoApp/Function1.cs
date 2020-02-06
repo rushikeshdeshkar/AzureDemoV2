98c99c0fef3b702be9ea1e4ec1401f048422981b
Rushikesh Deshkar <rushikesh.deshkar@transfast.com>
2020-02-06 15:25:08 +0530

    fix2

--- DemoApp/DemoApp/Function1.cs
+++ DemoApp/DemoApp/Function1.cs
@@ -8,7 +8,7 @@ namespace DemoApp
     public static class Function1
     {
         [FunctionName("Function1")]
-        public static void Run([ServiceBusTrigger("sendertopic", "testsubscription", Connection = "Endpoint=sb://rushiservicebus.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=RL+uE/KwEL90PmqRt2yiKqpaL/ATW5E2+WGyFVDW8ME=")]string mySbMsg, ILogger log)
+        public static void Run([ServiceBusTrigger("mytopic", "mysubscription", Connection = "")]string mySbMsg, ILogger log)
         {
             log.LogInformation($"C# ServiceBus topic trigger function processed message: {mySbMsg}");
         }
